d2e5e34b562b9007c3239d58a925f27b47bc154e
ItsKeyEnd2242 <pedrorumoaotopo@gmail.com>
2024-01-27 14:44:20 -0300

    Added default vue file with comments

---
+++ src/pages/user-welcome/UserWelcomePage.vue
@@ -0,0 +1,32 @@
+<template>
+    <!-- AQUI VAI OS COMPONENTES HTML -->
+</template>
+
+<style lang="scss" scoped>
+ /* AQUI VAI O ESTILO CSS DESTA PÁGINA EXCLUSIVAMENTE */
+</style>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default {
+    components: {
+        //BOTÕES/COMPONENTES CUSTOMIZADOS
+    },
+    props: {
+        //SUAS PROPRIEDADES
+    },
+    data () {
+        // INICIALIZAÇÃO DESTA PÁGINA
+        return {
+            variable: ref()
+        }
+    },
+    methods: {
+        // FUNÇÕES/MÉTODOS
+    },
+    computed: {
+        // FUNÇÕES COMPUTADAS (FICAM EM CACHE NO NAVEGADOR PARA MELHORAR A PERFORMANCE)
+    }
+}
+</script>
