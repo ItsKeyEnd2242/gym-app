566bc3c277fb0fc2bcf80b6479569cfec213bcef
ItsKeyEnd2242 <pedrorumoaotopo@gmail.com>
2024-02-03 20:13:18 -0300

    applied  q-expansion then th q-item inside

--- src/pages/user-welcome/UserWelcomePage.vue
+++ src/pages/user-welcome/UserWelcomePage.vue
@@ -1,27 +1,39 @@
 <!-- AQUI VAI OS COMPONENTES HTML -->
 <template>
   <div class="q-pa-md">
-    <q-layout view="lHh Lpr lff" container style="height: 95vh" class="shadow-2 rounded-borders">
+    <q-layout view="lHh Lpr lff" container style="height: 90vh" class="shadow-2 rounded-borders">
       <q-header elevated class="bg-cyan-8">
         <q-toolbar>
           <q-btn flat @click="drawer = !drawer" round dense icon="menu" />
           <q-toolbar-title>Menu</q-toolbar-title>
         </q-toolbar>
-        
+
       </q-header>
-      
-      <q-drawer v-model="drawer" show-if-above :width="250" :breakpoint="400" bordered>
+
+      <q-drawer v-model="drawer" show-if-above :width="250" :breakpoint="0" :height="250" bordered>
         <q-list>
-          <q-item class="bg-primary text-white" clickable v-ripple>
+
+        <q-expansion-item
+        expand-separator
+        icon="mail"
+        label="Inbox"
+        caption="5 unread emails"
+        default-opened>
+        <q-item class="bg-primary text-white" clickable v-ripple>
             <q-item-section avatar><q-icon name="mdi-dumbbell"/></q-item-section>
             <q-item-section>Treinos</q-item-section>
           </q-item>
-
-          <q-item class="bg-secondary text-white" clickable v-ripple>
-            <q-item-section avatar><q-icon name="mdi-silverware-fork-knife"/></q-item-section>
-            <q-item-section>Dieta</q-item-section>
+          <q-item class="bg-accent text-white" clickable v-ripple>
+            <q-item-section avatar><q-icon name="mdi-dumbbell"/></q-item-section>
+            <q-item-section>Treinos</q-item-section>
           </q-item>
-        </q-list>
+        </q-expansion-item>
+        
+        <q-fab color="secondary" push icon="keyboard_arrow_right" direction="right">
+        <q-fab-action color="primary" @click="onClick" icon="mail" />
+        <q-fab-action color="accent" @click="onClick" icon="alarm" />
+      </q-fab>
+      </q-list>
       </q-drawer>
     </q-layout>
   </div>
